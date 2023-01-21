3768d64ed5330eb467b58106db88f2a8d492d8f0
BiliksuunSamuel <samuel.biliksuun@hubtel.com>
2023-01-21 15:32:58 +0000

    mongo connection upate

--- src/configuration/index.ts
+++ src/configuration/index.ts
@@ -1,6 +1,8 @@
 export default () => ({
   Port: parseInt(process.env.PORT) || parseInt(process.env.Port),
-  DatabaseUrl: process.env.DatabaseUrl,
+  DatabaseUrl:
+    process.env.DatabaseUrl ||
+    'mongodb+srv://samuelbills:77045109@cluster0.nakki.mongodb.net/HMSTAN',
   Database: process.env.Database,
   SmsKey: process.env.SmsKey,
   SmsUrl: process.env.SmsUrl,
